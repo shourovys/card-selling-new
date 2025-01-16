aee5a01024e9abc8dd8ad5c95451e8dddea7f61c
Shourov Saha <shourovys@gmail.com>
2025-01-17 00:16:51 +0600

    refactor: remove unused components and styles
    - Deleted unused CSS file `App.css`.
    - Removed several components including `ThemeProvider`, `Layout`, `DashboardLayout`, and various UI components (e.g., `Button`, `Input`, `Table`, etc.) to streamline the codebase.
    - Simplified `App` component by removing unnecessary providers and directly rendering the router.
    - Cleaned up navigation configuration by removing the `navigationItems` definition.
    - Removed related hooks and utility functions that are no longer in use.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { Layout } from '@/components/layout/Layout';
-import { ThemeProvider } from '@/components/theme-provider';
-import { queryClient } from '@/lib/query';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme='light' storageKey='ui-theme'>
-        <Router>
-          <Layout>hello</Layout>
-        </Router>
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+  return <Router>hello</Router>;
 }
 
 export default App;
